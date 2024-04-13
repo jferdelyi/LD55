f311b22a7567ba0355dc9f4d61ca69762ec84ffb
Jean-François Erdelyi <jferdelyi@gmail.com>
2024-04-13 15:40:27 +0200

    [FCT] Add creatures init

--- scripts/score.gd
+++ scripts/score.gd
@@ -22,7 +22,7 @@ func start():
 	$ScoreImage.show()
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	pass
 
 # Update score
