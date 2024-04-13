17c164de254f77e1cda0403066298cdcfa77d1eb
Civ92 <166902557+Civ92@users.noreply.github.com>
2024-04-13 18:02:34 +0200

    Try to implement chimera signal logic (FIXME)

--- scripts/pentagram.gd
+++ scripts/pentagram.gd
@@ -2,11 +2,15 @@ extends Node
 
 var summons_container = {}
 
-signal chimera_available()
+#signal chimera_available()
 
 func _ready():
+	print("coucou")
 	for type in Global.summons:
 		summons_container[type] = []
+	spawn_summons(Global.summons.Cat, 1)
+	spawn_summons(Global.summons.Spider, 1)
+		
 	
 # Spawn a given numbers of summons of selected type
 # Returns true if successful
@@ -14,11 +18,17 @@ func spawn_summons(type : Global.summons, count : int) -> bool:
 	var ret = true
 	for i in range(0, count):
 		ret = ret and _spawn_one_summon(type)
+	check_chimera_availability()
 	return ret
 		
 # Spawn one summon of a given type
 # Returns true if successful
 func _spawn_one_summon(type : Global.summons) -> bool:
+	#HELP Jean-François ! 
+	#Je ne sais pas pourquoi mais cette ligne ne fonctionne plus
+	#Je pense qu'il caste en int ou je ne sais quoi....
+	#Pour infos cette ligne etait good avant que je passe le enum summons dans global...
+	#si t'as une idée...
 	if type in Global.summons:
 		var new_summon = Creature.new()
 		summons_container[type].append(new_summon)
@@ -37,6 +47,7 @@ func destroy_summons(type, count) -> bool:
 		return false
 	for i in range(0, count):
 		ret = ret and _destroy_one_summon(type)
+	check_chimera_availability()
 	return ret	
 
 # Destroy one summon of given type
@@ -65,4 +76,15 @@ func get_number_of_all_summons() -> int:
 		ret += get_number_of_summons(type)
 	return ret
 	
-	
+func check_chimera_availability():
+	#TODO : FIXME
+	for chimera in Global.summons_requirements.keys():
+		var is_available = true
+		for creature in Global.summons_requirements[chimera].keys():	
+			var count = get_number_of_summons(creature)
+			var enough_creature = count >= Global.summons_requirements[chimera][creature]
+			#print("Creature # : " + str(creature) + "has count of :" + str(count))
+			is_available = is_available and enough_creature
+		#print(chimera)
+		#print(is_available)
+		#emit_signal("chimera_available", chimera, is_available)	
