59eb9f47758e7f9e6a248bf9cdd649fcfff1e8c6
Civ92 <166902557+Civ92@users.noreply.github.com>
2024-04-13 16:05:14 +0200

    Add spaw and remove summons method, fix script in pentagram

--- scripts/pentagram.gd
+++ scripts/pentagram.gd
@@ -4,10 +4,63 @@ enum summons {Cat, Spider, Mouse, Chimera1, Chimera2, Chimera3, Demon}
 
 var summons_container = {}
 
-
-func spawn_summons(type, count) -> void:
-	print("Grrr")
+func _ready():
+	for type in summons:
+		summons_container[type] = []
 	
-func destroy_summons(type, count) -> void:
-	print("Eurf")
+# Spawn a given numbers of summons of selected type
+# Returns true if successful
+func spawn_summons(type : summons, count : int) -> bool:
+	var ret = true
+	for i in range(0, count):
+		ret = ret and _spawn_one_summon(type)
+	return ret
+		
+# Spawn one summon of a given type
+# Returns true if successful
+func _spawn_one_summon(type : summons) -> bool:
+	if type in summons:
+		var new_summon = Creature.new()
+		summons_container[type].append(new_summon)
+		add_child(new_summon)
+		print("Grrr")
+		return true
+	else:
+		print("plouf")
+		return false	
 
+# Destroy a given number of summons of given type
+# Returns true if successful
+func destroy_summons(type, count) -> bool:
+	var ret = true;
+	if get_number_of_summons(type) < count :
+		return false
+	for i in range(0, count):
+		ret = ret and _destroy_one_summon(type)
+	return ret	
+
+# Destroy one summon of given type
+# Returns true if successful		
+func _destroy_one_summon(type) -> bool:
+	#TODO : kill a random instead of oldest ?
+	if type in summons and summons_container[type].size() > 0:
+		var to_kill = summons_container[type][0]
+		remove_child(to_kill)
+		summons_container[type].erase(to_kill)
+		print("Ded")
+		return true
+	print("not ded")
+	return false
+
+#Returns the number of summons of a given type
+func get_number_of_summons(type) -> int:
+	if (type in summons_container.keys()):
+		return summons_container[type].size()
+	return 0
+	
+# Returns the total number of all summons
+func get_number_of_all_summons() -> int:
+	var ret := 0 
+	for type in summons:
+		ret += get_number_of_summons(type)
+	return ret
