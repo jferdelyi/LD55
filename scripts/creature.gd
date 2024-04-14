c6c673eac4bc0f6ab2053f06f983db4a4b0d2be7
Civ92 <166902557+Civ92@users.noreply.github.com>
2024-04-14 11:36:30 +0200

    Add a random move mecanic for creatures

--- scripts/creature.gd
+++ scripts/creature.gd
@@ -1,3 +1,48 @@
 extends Node2D
 class_name Creature
 
+@export var move_time := 3
+@export var move_speed_factor := 100
+@export var move_delay := 20
+
+var _last_move_tick = 0.0
+var _move_direction = Vector2(0,0)
+var _move_speed_px = 500
+
+func _ready():
+	#Set a random begin time to move
+	_last_move_tick = Time.get_ticks_msec() - randi_range(0, move_delay) * 1000
+	
+func _process(delta):
+	move_around(delta)
+	
+func move_around(delta) -> void:
+	var is_moving = Time.get_ticks_msec() - _last_move_tick < move_time * 1000
+	var can_move = Time.get_ticks_msec() - _last_move_tick > move_delay * 1000
+	
+	if is_moving:
+		_continue_moving(delta)
+	elif can_move:
+		_initiate_move()
+	else:
+		_stop_moving()
+		
+func _continue_moving(delta) -> void:
+	print(position)
+	position += _move_direction * move_speed_factor * delta
+	print(position)
+	
+func _initiate_move() -> void:
+	_last_move_tick = Time.get_ticks_msec()
+	var vx = randi_range(- _move_speed_px, _move_speed_px)
+	var vy = randi_range(-_move_speed_px, _move_speed_px)
+	if Vector2(vx, vy) != Vector2(0,0):
+		_move_direction = Vector2(vx, vy).normalized()
+	print(_move_direction)	
+	
+
+func _stop_moving() -> void:
+	_move_direction = Vector2(0,0)
+	
+
+
